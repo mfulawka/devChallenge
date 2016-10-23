2bf16d66b294d2dc1fc744a7a301c6d166e18a1e
MFULAWKA <mateusz.fulawka@capgemini.com>
2016-10-23 17:49:12 +0200

    test script fixed

--- tests/simple_test.sh
+++ tests/simple_test.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
+
 sleep 5
-{
-    curl -s "localhost:8088/api/direct?dep_sid=3&arr_sid=4" | grep -E 'true|false' > /dev/null &&
-    curl -s "localhost:8088/api/direct?dep_sid=0&arr_sid=1" | grep -E 'true|false' > /dev/null &&
-    echo TEST PASSED!
-} || {
-    echo TEST FAILED!
-}
+IP1=`curl -s "localhost:8080/api/direct?dep_sid=3&arr_sid=4" | grep -E 'true|false'`
+IP2=`curl -s "localhost:8080/api/direct?dep_sid=0&arr_sid=1" | grep -E 'true|false'`
+if [ -n "$IP1" ] && [ -n "$IP2" ]; then   
+echo "TEST PASSED!"
+else echo "TEST FAILED!"
+fi
